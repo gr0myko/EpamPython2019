7353f3af33c5b4ef71069dd6d76018b0036a21ce
gr0myko <57479809+gr0myko@users.noreply.github.com>
2019-11-18 22:12:36 +0300

    Update homework_strings.py

--- 01-Data-Structures/hw/carrots/homework_strings.py
+++ 01-Data-Structures/hw/carrots/homework_strings.py
@@ -33,26 +33,57 @@ P.S. За незакрытый файловый дескриптор - кара�
 
 """
 
-# read the file dna.fasta
-dna = None
-
+dna = open('./files/dna.fasta')
+#creating dictionary for dna names and dna sequences
+dna_seqs = {}
+for line in dna:
+    line = line.rstrip()
+    if line[0] == '>':
+        name_words = line.split()
+        name = name_words[0][1:]
+        dna_seqs[name] = ''
+    else:
+        dna_seqs[name] = dna_seqs[name] + line
+dna.close()
 
 def translate_from_dna_to_rna(dna):
-    
-    """your code here"""
-    
+    rna = {}
+    for dna_id, seq in dna.items():
+        rna[dna_id] = seq.replace('T', 'U')
     return rna
 
 
+
 def count_nucleotides(dna):
-    
-    """your code here"""
-    
+    num_of_nucleotides = []
+    for dna_id, seq in dna.items():
+        nucleotid_dict = {}
+        for nucleotid in seq:
+            nucleotid_dict[nucleotid] = seq.count(nucleotid)
+        num_of_nucleotides.append(nucleotid_dict)
     return num_of_nucleotides
 
 
+
+rna_codon = open('./files/rna_codon_table.txt')
+tr_dict = {}
+for element in rna_codon.read().split():
+    if len(element) == 3:
+        name = element
+        tr_dict[name] = ''
+    else:
+        tr_dict[name] += element
+rna_codon.close()
+rna = translate_from_dna_to_rna(dna_seqs)
+
+
+
 def translate_rna_to_protein(rna):
-    
-    """your code here"""
-    
+    protein = ''
+    for key, rna_seq in rna.items():
+        for i in range(0, len(rna_seq), 3):
+            if tr_dict[rna_seq[i:i+3]] == 'Stop':
+                break
+            else:
+                protein += tr_dict[rna_seq[i:i+3]]
     return protein
