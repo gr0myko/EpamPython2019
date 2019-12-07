e5f6e67c8f9f4f96dc887e84d466784f5062f7ea
gr0myko <57479809+gr0myko@users.noreply.github.com>
2019-12-07 14:01:47 +0300

    Delete hw4_projecteuler.py

--- 03-fp-decorator/hw/hw4_projecteuler.py
+++
@@ -1,7 +0,0 @@
-from functools import reduce
-s = ''
-for i in range(0, 1000000):
-    s = s + str(i)
-list_from_s = [int(value) for pos, value in enumerate(s) if pos in {1, 10, 100, 1000, 10000, 100000, 1000000}]
-b = reduce(lambda x, y: x*y, list_from_s)
-print(b)
