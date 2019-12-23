664cbb65da7505fa9231bb0593888368fb2ab6ba
gr0myko <57479809+gr0myko@users.noreply.github.com>
2019-12-23 10:55:01 +0300

    Add files via upload

--- 10-Design_Patterns/hw/1-Decorator/decorator.py
+++ 10-Design_Patterns/hw/1-Decorator/decorator.py
@@ -14,6 +14,38 @@ class BaseCoffe(Component):
         return 90
 
 
+class Decorator(Component):
+    def __init__(self, decorated_coffee):
+        self.decorated_coffee = decorated_coffee
+
+    def get_cost(self):
+        return self.decorated_coffee.get_cost()
+
+
+class Whip(Decorator):
+    def __init__(self, decorated_coffee):
+        super(Whip, self).__init__(decorated_coffee)
+
+    def get_cost(self):
+        return self.decorated_coffee.get_cost() + 4.25
+
+
+class Marshmallow(Decorator):
+    def __init__(self, decorated_coffee):
+        super(Marshmallow, self).__init__(decorated_coffee)
+
+    def get_cost(self):
+        return self.decorated_coffee.get_cost() + 7
+
+
+class Syrup(Decorator):
+    def __init__(self, decorated_coffee):
+        super(Syrup, self).__init__(decorated_coffee)
+
+    def get_cost(self):
+        return self.decorated_coffee.get_cost() + 3.85
+
+
 if __name__ == "__main__":
     coffe = BaseCoffe()
     coffe = Whip(coffe)
