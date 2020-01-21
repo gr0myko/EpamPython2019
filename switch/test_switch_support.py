3c454536b318a57f41a2ecc43adb582c769e963c
gr0myko <57479809+gr0myko@users.noreply.github.com>
2020-01-21 18:40:04 +0300

    Add files via upload

---
+++ switch/test_switch_support.py
@@ -0,0 +1,12 @@
+import unittest
+from switch_support import test_function
+
+
+class SwitchSupportTests(unittest.TestCase):
+    def test_expr_in_argument(self):
+        result = test_function(11, 9 + 2, 16, 5)
+        self.assertEqual(result, '1')
+
+    def test_expr_in_docstring(self):
+        result = test_function(5 * 3, 11, 13, 16)
+        self.assertEqual(result, '4')
